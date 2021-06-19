a0c33661da46429a4a0efcff7d03bf2019506b2c
EAKYOUNG YU <eakyoungyu@gmail.com>
2021-06-20 03:26:57 +0900

    Add Room app

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",  # Title of the blue sqaure
+            {
+                "fields": (  # Inside of the blue square
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
